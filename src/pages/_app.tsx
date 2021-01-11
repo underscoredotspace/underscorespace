1778f744e2fcb0ce96476e30357506e1b1d9719d
Colin Tindle <colin.tindle@fanduel.com>
2021-01-11 11:44:22 +0000

    move prism css to npm/import so it can be pruned

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/dist/next-server/lib/router/router"
 import "styles/style.css"
 import Head from "next/head"
+import "prism-theme-night-owl/build/no-italics.css"
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
     <>
@@ -10,10 +11,6 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
-            />
-            <link
-                href="https://unpkg.com/prism-theme-night-owl@1.4.0/build/no-italics.css"
-                rel="stylesheet"
             />
         </Head>
         <Component {...pageProps} />
