f3f7d3cef8af6cb63389da58f70d3d434d2eeea4
Colin Tindle <colin.tindle@fanduel.com>
2021-01-12 12:30:52 +0000

    color and twitter image (seems to be required)

--- src/components/SocialCard.tsx
+++ src/components/SocialCard.tsx
@@ -10,6 +10,8 @@ const SocialCard: React.FC<SocialCardProps> = ({ title = "Home" }) => (
         <meta property="og:description" content={DESCIRIPTION} />
         <meta name="twitter:card" content="summary" />
         <meta name="og:image" content="/colon.png" />
+        <meta name="twitter:image" content="/colon.png" />
+        <meta name="theme-color" content="#ff69b4" />
     </>
 )
 
