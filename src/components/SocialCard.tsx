a56f6d442e8cefc7e47ecffb06b6fcc261082080
Colin Tindle <colin.tindle@fanduel.com>
2021-01-12 12:23:23 +0000

    remove duplication

--- src/components/SocialCard.tsx
+++ src/components/SocialCard.tsx
@@ -7,14 +7,8 @@ const DESCIRIPTION = "underscore.space - a JavaScript blog by Colin Tindle"
 const SocialCard: React.FC<SocialCardProps> = ({ title = "Home" }) => (
     <>
         <meta property="og:title" content={title} />
-        <meta name="twitter:title" content={title} />
-
         <meta property="og:description" content={DESCIRIPTION} />
-        <meta property="twitter:description" content={DESCIRIPTION} />
-
         <meta name="twitter:card" content="summary" />
-
-        <meta name="twitter:image" content="/colon.png" />
         <meta name="og:image" content="/colon.png" />
     </>
 )
